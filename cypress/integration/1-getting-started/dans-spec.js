3f4d9f224e2a9fb70e71b998635274f5dc116f5d
Daniel Giordano <danielgiordano@Daniels-MacBook-Pro.local>
2022-03-28 19:27:17 -0400

    eyes

---
+++ cypress/integration/1-getting-started/dans-spec.js
@@ -0,0 +1,19 @@
+describe('My First Test', () => {
+    it('Visits the Kitchen Sink', () => {
+      cy.visit('https://nextjs-v2-theta.vercel.app/')
+      cy.contains("Welcome to Applitools Next")
+      cy.contains("Documentation")
+      cy.eyesOpen({
+        appName: 'Next App',
+        testName: 'Hero Headline',
+    })
+
+      cy.eyesCheckWindow({
+        tag: "Homepage",
+        target: 'window',
+        fully: true
+    });
+
+      cy.eyesClose()
+    });
+});
